3743d2139aaefecd54396087e2c7adc39540886b
The legend Ahmad <ahmadsanad135@gmail.com>
2025-12-19 12:53:37 +0300

    Update surahLinks.js

--- surahLinks.js
+++ surahLinks.js
@@ -1,1 +1,29 @@
-const surahLinks = { "afasy": [ "https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/001.mp3", "https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/002.mp3", "https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/003.mp3", // … أكمل حتى 114 ], "maher": [ "https://download.quranicaudio.com/quran/maher_maiqili/001.mp3", "https://download.quranicaudio.com/quran/maher_maiqili/002.mp3", "https://download.quranicaudio.com/quran/maher_maiqili/003.mp3", // … أكمل حتى 114 ], "yasir": [ "https://download.quranicaudio.com/quran/yasir_al-dosari/001.mp3", "https://download.quranicaudio.com/quran/yasir_al-dosari/002.mp3", "https://download.quranicaudio.com/quran/yasir_al-dosari/003.mp3", // … أكمل حتى 114 ], "sudais": [ "https://download.quranicaudio.com/quran/abdulrahman_sudais/001.mp3", "https://download.quranicaudio.com/quran/abdulrahman_sudais/002.mp3", "https://download.quranicaudio.com/quran/abdulrahman_sudais/003.mp3", // … أكمل حتى 114 ], "eslam": [ "https://download.quranicaudio.com/quran/eslam_sobhi/001.mp3", "https://download.quranicaudio.com/quran/eslam_sobhi/002.mp3", "https://download.quranicaudio.com/quran/eslam_sobhi/003.mp3", // … أكمل حتى 114 ] };
+const surahLinks = {
+  "afasy": [
+    "https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/001.mp3",
+    "https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/002.mp3",
+    "https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/003.mp3"
+    // … أكمل باقي الروابط حتى 114 لكل قارئ
+  ],
+  "maher": [
+    "https://download.quranicaudio.com/quran/maher_maiqili/001.mp3",
+    "https://download.quranicaudio.com/quran/maher_maiqili/002.mp3",
+    "https://download.quranicaudio.com/quran/maher_maiqili/003.mp3"
+    // …
+  ],
+  "yasir": [
+    "https://download.quranicaudio.com/quran/yasir_al-dosari/001.mp3",
+    "https://download.quranicaudio.com/quran/yasir_al-dosari/002.mp3"
+    // …
+  ],
+  "sudais": [
+    "https://download.quranicaudio.com/quran/abdulrahman_sudais/001.mp3",
+    "https://download.quranicaudio.com/quran/abdulrahman_sudais/002.mp3"
+    // …
+  ],
+  "eslam": [
+    "https://download.quranicaudio.com/quran/eslam_sobhi/001.mp3",
+    "https://download.quranicaudio.com/quran/eslam_sobhi/002.mp3"
+    // …
+  ]
+};
