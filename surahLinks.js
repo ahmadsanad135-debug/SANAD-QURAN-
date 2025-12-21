51abb30fc89f80352e9f19a56226bee383b7994c
The legend Ahmad <ahmadsanad135@gmail.com>
2025-12-21 11:20:42 +0300

    Create surahLinks.js

---
+++ surahLinks.js
@@ -0,0 +1,14 @@
+const surahLinks = {
+  afasy: [],
+  maher: []
+};
+
+for (let i = 1; i <= 114; i++) {
+  surahLinks.afasy.push(
+    `https://cdn.islamic.network/quran/audio-surah/128/ar.alafasy/${i}.mp3`
+  );
+
+  surahLinks.maher.push(
+    `https://cdn.islamic.network/quran/audio-surah/128/ar.mahermuaiqly/${i}.mp3`
+  );
+}
